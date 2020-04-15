44b24de1e842150c8ddf064dc12de7b97b2fdfec
kumar saharsh <32514257+Saharsh007@users.noreply.github.com>
2020-04-15 23:51:22 +0530

    Add files via upload
    my test file

---
+++ test/test.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define f(i,a,b) for(long long i=a;i<b;i++)
+#define pb push_back
+#define ll long long 
+#define debug3(a,b,c) cout<<a<<"->"<<b<<"->"<<c<<"\n"
+#define debug4(a,b,c,d) cout<<a<<"->"<<b<<"->"<<c<<"->"<<d<<"\n"
+#define printarray(A) for(long long i=0; i<A.size() ; i++) cout<<A[i]<<" "; cout<<"\n";
+#define mod 1000000007
+
+//space and time pass
+// vector<int> v(30); int tt=20; while(tt) tt--;
+
+
+int main() 
+{ 
+	string s = "hello";
+	cout<<s.substr(0,1);
+}
+
+1. When will we get our managers assigned?
+
+2. Will we get option to choose projects? I am not too good with development work as I've never really done much of developent but I've been working on deep learning for a long time and would love to have a project related to it.
+
+3. Our college is dated to open on 1st June. In the case of lockdown extension can the internship also be extended? We can do more work and it'll be easy for you to evaluate too.
+
+4. Our internship duration is from 20th April to 29th May, that's 39 days. Isn't it too less time for us to work? On top of that, if we are unfamiliar with the technology stack, it'll take another few days to get familiarized with that. In that case what will be the evaluation criteria for PPO. Will DSA be there or just the work we do in the internship duration.
+
+5. This ain't a query but praise. The internship is not even started and I'm aleady in love how amazon works. No other company I know have managed interns as smoothly as amazon did. Some have cancelled and many others are still in decision making phase. Thank you.
